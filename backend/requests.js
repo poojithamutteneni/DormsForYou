1439fa1ad1745c9253b17aaba7df4e9499ab5783
Revanth <rev@pop-os.localdomain>
2021-03-30 17:02:57 +0530

    CleanUp

--- backend/requests.js
+++ backend/requests.js
@@ -1,20 +1,11 @@
 //Import
 const express = require('express');
-const bodyParser = require('body-parser')
 require("dotenv").config();
-var jwt = require('jsonwebtoken');
-const { Pool, Client } = require('pg');
+const { Pool } = require('pg');
 const tokenauth = require("./token");
-
-
 
 //Initialize
 const app = express.Router();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-
-
 
 //db connection
 const pool = new Pool({
@@ -24,8 +15,6 @@ const pool = new Pool({
     password: "password",
     port: 5432
 });
-
-
 
 
 //get colleges
@@ -41,8 +30,6 @@ try{
 })
 
 
-
-
 //get hostels by name
 app.post('/gethostelsbyname', async (req,res) =>{
 let name = req.headers["name"]+ "%";
@@ -55,11 +42,6 @@ try{
     console.log(error);
   }
 })
-
-
-
-
-
 
 //get hostels by cid
 app.post('/gethostelsbycid', (req,res) =>{
@@ -77,9 +59,6 @@ app.post('/gethostelsbycid', (req,res) =>{
 })
 
 
-
-
-
 //getHostelsNearbyCurrentLocation
 app.post('/getHostelsNearbyCurrentLocation', async (req,res) =>{
   let minlat =  parseFloat(req.headers["latitude"]) - 0.5;
@@ -87,24 +66,26 @@ app.post('/getHostelsNearbyCurrentLocation', async (req,res) =>{
   let minlon =  parseFloat(req.headers["longitude"]) - 0.5;
   let maxlon =  parseFloat(req.headers["longitude"]) + 0.5;
   let hostels;
+  
   try{
-  for(let i = 0; i <= 2; ){
+  while(1){
     let r = await pool.query("Select * from hostels where latitude >= $1 AND latitude <= $2 AND longitude >= $3 AND longitude <= $4; ", [minlat, maxlat, minlon,maxlon]);
     hostels = (r.rows);
     
-    if(r.rows.length > 0) i++;
-    // console.log(minlat + " " + maxlat);
-      minlat -= 0.5;  maxlat += 0.5; minlon -= 0.5; maxlon += 0.5;
+    if(r.rows.length >= 2){
+      break;
+    }
+      minlat -= 0.5;
+      maxlat += 0.5;
+      minlon -= 0.5;
+      maxlon += 0.5;
       }
       res.send(hostels);
-    // console.log(hostels.length);
 }
 catch(error){
   console.log(error);
 }
 });
-
-
 
 //booking
 app.post('/book', tokenauth, (req,res) =>{
